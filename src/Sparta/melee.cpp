2f2c09fb7da2fbb7fe1f03163637df0169f60548
Ghoussein Edouardo <edouardo.ghoussein@student-cs.fr>
2024-02-17 14:16:29 +0100

    AAMFOMR

--- src/Sparta/melee.cpp
+++ src/Sparta/melee.cpp
@@ -1,18 +1,33 @@
 #include "melee.h"
 
-void SpartanMode(Gladiator *gladiator, Position ally_position){
-    uint8_t team = gladiator->robot->getData().teamId;
-    Position current = gladiator->robot->getData().position;
-    float dx = current.x - ally_position.x;
-    float dy = current.y - ally_position.y;
+bool AttackPlayer(RobotData player, RobotData enemy){
+    if(enemy.lifes <= 0) return false;
+    Position current = player.position;
+    Position er1 = enemy.position;
+    float dx = current.x - er1.x;
+    float dy = current.y - er1.y;
 
     float distance = sqrt(dx*dx + dy*dy);
 
     if(distance < ATTACK_DISTANCE){
+        float orientation = atan2(dy, dx);
 
+        if(er1.a >= -orientation - PI/2 && er1.a <= -orientation + PI/2){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void SpartanMode(GameState *game){
+    if(AttackPlayer(game->myData, game->er1Data)){
+        game->goal = game->er1Data.position;
+    } else if(AttackPlayer(game->myData, game->er2Data)){
+        game->goal = game->er2Data.position;
     }
     else{
-        gladiator->control->setWheelSpeed(WheelAxis::LEFT, 1);
-        gladiator->control->setWheelSpeed(WheelAxis::RIGHT, -1);
+        game->gladiator->control->setWheelSpeed(WheelAxis::LEFT, 1);
+        game->gladiator->control->setWheelSpeed(WheelAxis::RIGHT, -1);
     }
 }
--- src/Sparta/melee.h
+++ src/Sparta/melee.h
@@ -1,6 +1,7 @@
 #pragma once
-#include "gladiator.h"
+#include "GameData\GameData.h"
 
 #define ATTACK_DISTANCE 3
 
-void SpartanMode(Gladiator *gladiator);
+bool AttackPlayer(RobotData player, RobotData enemy);
+void SpartanMode(GameState *game);
