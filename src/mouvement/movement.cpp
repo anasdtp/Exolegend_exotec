828634b0ea7538c11c685cb22e67239bfdfb9997
Ghoussein Edouardo <edouardo.ghoussein@student-cs.fr>
2024-02-17 02:36:23 +0100

    AAAAA

---
+++ src/mouvement/movement.cpp
@@ -0,0 +1,62 @@
+#include "movement.h"
+
+float kw = 1.2;
+float kv = 1.f;
+float wlimit = 3.f;
+float vlimit = 0.3f;
+float erreurPos = 0.07;
+
+float squareSize;
+
+Position getSquareCoor(const MazeSquare *square){
+    return getSquareCoor(square->i, square->j);
+}
+
+Position getSquareCoor(uint8_t i, uint8_t j){
+    Position coor;
+
+    coor.x = (i + 0.5) * squareSize;
+    coor.y = (j + 0.5) * squareSize;
+
+    return coor;   
+}
+
+float distance(const Position &p1, const Position &p2){
+    return sqrt(pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2));
+}
+
+double reductionAngle(double x)
+{
+    x = fmod(x + PI, 2 * PI);
+    if (x < 0)
+        x += 2 * PI;
+    return x - PI;
+}
+void go_to(Position cons, Position pos, Gladiator *gladiator)
+{
+    double consvl, consvr;
+    double dx = cons.x - pos.x;
+    double dy = cons.y - pos.y;
+    double d = sqrt(dx * dx + dy * dy);
+
+    if (d > erreurPos)
+    {
+        double rho = atan2(dy, dx);
+        double consw = kw * reductionAngle(rho - pos.a);
+
+        double consv = kv * d * cos(reductionAngle(rho - pos.a));
+        consw = abs(consw) > wlimit ? (consw > 0 ? 1 : -1) * wlimit : consw;
+        consv = abs(consv) > vlimit ? (consv > 0 ? 1 : -1) * vlimit : consv;
+
+        consvl = consv - gladiator->robot->getRobotRadius() * consw; // GFA 3.6.2
+        consvr = consv + gladiator->robot->getRobotRadius() * consw; // GFA 3.6.2
+    }
+    else
+    {
+        consvr = 0;
+        consvl = 0;
+    }
+
+    gladiator->control->setWheelSpeed(WheelAxis::RIGHT, consvr, false); // GFA 3.2.1
+    gladiator->control->setWheelSpeed(WheelAxis::LEFT, consvl, false);  // GFA 3.2.1
+}
